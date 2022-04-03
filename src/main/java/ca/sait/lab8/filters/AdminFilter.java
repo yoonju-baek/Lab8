5717749ea56995ce1303eec1959c23bbe138b32c
Yoonju Baek <mbibe91@gmail.com>
2022-04-02 21:58:34 -0600

    add adminfilter

---
+++ src/main/java/ca/sait/lab8/filters/AdminFilter.java
@@ -0,0 +1,59 @@
+package ca.sait.lab8.filters;
+
+import ca.sait.lab8.models.User;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.*;
+
+/**
+ *
+ * @author Yoonju Baek
+ */
+public class AdminFilter implements Filter {
+
+    /**
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain The filter chain we are processing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        
+        HttpSession session = httpRequest.getSession();
+        User user = (User) session.getAttribute("user");
+                
+        // Check if user if an admin or not
+        // If admin:
+        if(user.getRole().getRoleId() == 1) {
+           chain.doFilter(request, response); 
+        }
+        // If not admin:
+        else {
+            httpResponse.sendRedirect("/notes");
+        }
+    }
+
+    /**
+     * Destroy method for this filter
+     */
+    public void destroy() {        
+    }
+
+    /**
+     * Init method for this filter
+     */
+    public void init(FilterConfig filterConfig) {        
+    }
+}
--- src/main/java/ca/sait/lab8/servlets/LoginServlet.java
+++ src/main/java/ca/sait/lab8/servlets/LoginServlet.java
@@ -39,6 +39,7 @@ public class LoginServlet extends HttpServlet {
         }
         
         HttpSession session = request.getSession();
+        session.setAttribute("user", user);
         session.setAttribute("email", email);
         
         if (user.getRole().getRoleId() == 1) {
